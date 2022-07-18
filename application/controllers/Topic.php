b3448eaaa9a5bb003c0b59ab489b8aad593464b8
ahmadhafis <ahmadhafis296@gmail.com>
2022-07-18 15:53:49 +0700

    Progress edit for topic

--- application/controllers/Topic.php
+++ application/controllers/Topic.php
@@ -1,65 +1,67 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Topic extends CI_Controller {
+class Topic extends CI_Controller
+{
 
-    public function __construct()
-    {
-        parent::__construct();
-        $this->load->model('topic_model','topic');
-    }
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('topic_model', 'topic');
+	}
 
-    public function index()
-    {
-        $data = array();
-        $data['topics'] = $this->topic->getTopic();
-        $this->load->view('topik/mainTopik', $data);
-    }
+	public function index()
+	{
+		$data = array();
+		$data['topics'] = $this->topic->getTopic();
+		$this->load->view('topik/mainTopik', $data);
+	}
 
-    public function form($topic_id = null)
-    {
-        $data = array();
-        if($topic_id){
-            $data['topic'] = $this->topic->getTopicById($topic_id);
-        }
-        $this->load->view('topik/createTopik', $data);
-    }
+	public function form($topic_id = null)
+	{
+		$data = array();
+		if ($topic_id) {
+			$data['topic'] = $this->topic->getTopicById($topic_id);
+		}
+		$this->load->view('topik/createTopik', $data);
+	}
 
-    public function save($id = null)
-    {
-        $form_data = array
-        (
-            // 'id' => $id,
-            'name' => $this->input->post('name')                      
-        );
-        if(!$id){
-            $send_form = $this->topic->createTopic($form_data);
-        } else {
-            $send_form = $this->topic->updateTopic($form_data);
-        }
+	public function save($id = null)
+	{
+		$form_data = array(
+			// 'id' => $id,
+			'name' => $this->input->post('name')
+		);
+		if (!$id) {
+			$send_form = $this->topic->createTopic($form_data);
+		} else {
+			$send_form = $this->topic->updateTopic($form_data);
+		}
 
-        if($send_form){
-            $this->session->set_flashdata('message', array('success','Data berhasil ditambahkan!'));
-            redirect('http://localhost/website-saw/');
-        }
-        else
-        {
-            $this->session->set_flashdata('message', array('danger','Ops! Dados incorretos!'));
-            redirect('topic/form');
-        }
-    }
+		if ($send_form) {
+			$this->session->set_flashdata('message', array('success', 'Data berhasil ditambahkan!'));
+			redirect('http://localhost/website-saw/');
+		} else {
+			$this->session->set_flashdata('message', array('danger', 'Ops! Dados incorretos!'));
+			redirect('topic/form');
+		}
+	}
 
-    public function delete($id)
-    {
-        $delete = $this->topic->deleteTopic($id);
-        if($delete){
-            $this->session->set_flashdata('message', array('success','Data berhasil dihapuskan!'));
-            redirect('topic');
-        }
-        else
-        {
-            $this->session->set_flashdata('message', array('danger','Ops! Produk tidak ditemukan!'));
-            redirect('topic');
-        }
-    }
+	public function delete($id)
+	{
+		$delete = $this->topic->deleteTopic($id);
+		if ($delete) {
+			$this->session->set_flashdata('message', array('success', 'Data berhasil dihapuskan!'));
+			redirect('topic');
+		} else {
+			$this->session->set_flashdata('message', array('danger', 'Ops! Produk tidak ditemukan!'));
+			redirect('topic');
+		}
+	}
+
+	public function edit($id)
+	{
+		$topic = $this->db->where(['id' => $id])->get('topic')->row();
+		$this->load->view('topik/editTopik', ['topic' => $topic]);
+	}
 }
