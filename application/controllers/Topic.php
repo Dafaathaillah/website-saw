2a2df5e463667d09cf9d3a906ec2683849fb5143
ahmadhafis <ahmadhafis296@gmail.com>
2022-07-21 12:27:02 +0700

    Pagination fix

--- application/controllers/Topic.php
+++ application/controllers/Topic.php
@@ -27,9 +27,34 @@ class Topic extends CI_Controller
 		$config['next_link'] = 'Next';
 		$config['prev_link'] = 'Prev';
 
-		$config['full_tag_open'] = '<div class="pagination justify-content-end"><nav><ul class"pagination">';
+		$choice = $config["total_rows"] / $config['per_page'];
+        $config["num_links"] = floor($choice);
+
+        $config['first_link'] = 'First';
+        $config['last_link'] = 'Last';
+        $config['next_link'] = 'Next';
+        $config['prev_link'] = 'Prev';
+
+		$config['full_tag_open'] = '<div class="pagination"><nav><ul class="pagination pagination-rounded">';
 		$config['full_tag_close'] = '</ul></nav></div>';
-		
+
+		$config['num_tag_open'] = '<li class="page-item"><span class="page-link">';
+		$config['num_tag_close'] = '</span></li>';
+
+		$config['cur_tag_open'] = '<li class="page-item active"><span class="page-link">';
+		$config['cur_tag_close'] = '</span></li>';
+
+		$config['next_tag_open'] = '<li class="page-item"><span class="page-link">';
+		$config['next_tag_close'] = '</span></li>';
+
+		$config['prev_tag_open'] = '<li class="page-item"><span class="page-link">';
+		$config['prev_tag_close'] = '</span></li>';
+
+		$config['first_tag_open'] = '<li class="page-item"><span class="page-link">';
+		$config['first_tag_close'] = '</span></li>';
+
+		$config['last_tag_open'] = '<li class="page-item"><span class="page-link">';
+		$config['last_tag_close'] = '</span></li>';
 
 		
 
