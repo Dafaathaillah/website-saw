34e54699163e4a491c0b60908f880e47493f3b8f
Dafa Bintang Athaillah <55053091+Dafaathaillah@users.noreply.github.com>
2022-07-21 11:58:48 +0800

    update sidebar

--- application/views/layouts/sidebar.php
+++ application/views/layouts/sidebar.php
@@ -60,12 +60,12 @@ License: You must have a valid license purchased only from above link or https:/
 						<span class="link-title">Perhitungan</span>
 					</a>
 				</li>
-				<li class="nav-item">
-					<a href="<?php base_url() ?> Hasil" class="nav-link">
-						<i class="link-icon" data-feather="pie-chart"></i>
-						<span class="link-title">Hasil</span>
-					</a>
-				</li>
+					<!-- <li class="nav-item">
+						<a href="<?php base_url() ?> Hasil" class="nav-link">
+							<i class="link-icon" data-feather="pie-chart"></i>
+							<span class="link-title">Hasil</span>
+						</a>
+					</li> -->
 			</ul>
 		</div>
 	</nav>
