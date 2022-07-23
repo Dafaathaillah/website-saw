9cb31001b69b4ef6aa119e7d97a420df24cde15d
ahmadhafis <ahmadhafis296@gmail.com>
2022-07-23 20:16:22 +0700

    update tableHasil

--- application/views/hasil/tableHasil.php
+++ application/views/hasil/tableHasil.php
@@ -28,6 +28,7 @@ License: You must have a valid license purchased only from above link or https:/
 	<link rel="stylesheet" href="assets/css/demo_1/style.css">
 	<!-- End layout styles -->
 	<link rel="shortcut icon" href="assets/images/favicon.png" />
+	<link rel="stylesheet" href="<?php echo base_url() ?>assets/vendors/datatables.net-bs4/dataTables.bootstrap4.css">
 </head>
 
 
@@ -42,26 +43,134 @@ License: You must have a valid license purchased only from above link or https:/
 							<table id="dataTableExample" class="table">
 								<thead>
 									<tr>
-										<th>Value</th>
-										<th>Data</th>
-										<th>Status</th>
+										<th rowspan="2">Alternatif</th>
+										<th colspan="6" class="text-center">Kriteria</th>
 									</tr>
-								</theadData>
+									<tr>
+										<th>Waktu Pengiriman</th>
+										<th>Jarak</th>
+										<th>Biaya</th>
+										<th>Armada</th>
+										<th>Garansi</th>
+									</tr>
+									</theadData>
 								<tbody>
 									<tr>
-										<td>Tiger Nixon</td>
-										<td>System Architect</td>
-										<td>Edinburgh</td>
+										<td>Alternatif A</td>
+										<td>1</td>
+										<td>0.5</td>
+										<td>1</td>
+										<td>0.5</td>
+										<td>1</td>
+										</td>
 									</tr>
 									<tr>
-										<td>Garrett Winters</td>
-										<td>Accountant</td>
-										<td>Tokyo</td>
+										<td>Alternatif B</td>
+										<td>2</td>
+										<td>3</td>
+										<td>2</td>
+										<td>3</td>
+										<td>2</td>
 									</tr>
 									<tr>
-										<td>Ashton Cox</td>
-										<td>Junior Technical Author</td>
-										<td>San Francisco</td>
+										<td>Alternatif C</td>
+										<td>0.3</td>
+										<td>0.5</td>
+										<td>1</td>
+										<td>5</td>
+										<td>4</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+					</div>
+					<div class="card-body">
+						<h6 class="card-title">Normalisasi</h6>
+						<div class="table-responsive">
+							<table id="dataTableExample" class="table">
+								<thead>
+									<tr>
+										<th rowspan="2">Alternatif</th>
+										<th colspan="6" class="text-center">Kriteria</th>
+									</tr>
+									<tr>
+										<th>Waktu Pengiriman</th>
+										<th>Jarak</th>
+										<th>Biaya</th>
+										<th>Armada</th>
+										<th>Garansi</th>
+									</tr>
+									</theadData>
+								<tbody>
+									<tr>
+										<td>Alternatif A</td>
+										<td>1</td>
+										<td>0.5</td>
+										<td>1</td>
+										<td>0.5</td>
+										<td>1</td>
+									</tr>
+									<tr>
+										<td>Alternatif B</td>
+										<td>2</td>
+										<td>3</td>
+										<td>2</td>
+										<td>3</td>
+										<td>2</td>
+									</tr>
+									<tr>
+										<td>Alternatif C</td>
+										<td>0.3</td>
+										<td>0.5</td>
+										<td>1</td>
+										<td>5</td>
+										<td>4</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+					</div>
+					<div class="card-body">
+						<h6 class="card-title">Perankingan</h6>
+						<div class="table-responsive">
+							<table id="dataTableExample" class="table">
+								<thead>
+									<tr>
+										<th rowspan="2">Alternatif</th>
+										<th colspan="6" class="text-center">Kriteria</th>
+									</tr>
+									<tr>
+										<th>Waktu Pengiriman</th>
+										<th>Jarak</th>
+										<th>Biaya</th>
+										<th>Armada</th>
+										<th>Garansi</th>
+									</tr>
+									</theadData>
+								<tbody>
+									<tr>
+										<td>Alternatif A</td>
+										<td>1</td>
+										<td>0.5</td>
+										<td>1</td>
+										<td>0.5</td>
+										<td>1</td>
+									</tr>
+									<tr>
+										<td>Alternatif B</td>
+										<td>2</td>
+										<td>3</td>
+										<td>2</td>
+										<td>3</td>
+										<td>2</td>
+									</tr>
+									<tr>
+										<td>Alternatif C</td>
+										<td>0.3</td>
+										<td>0.5</td>
+										<td>1</td>
+										<td>5</td>
+										<td>4</td>
 									</tr>
 								</tbody>
 							</table>
@@ -91,4 +200,6 @@ License: You must have a valid license purchased only from above link or https:/
 	<script src="assets/js/dashboard.js"></script>
 	<script src="assets/js/datepicker.js"></script>
 	<!-- end custom js for this page -->
+	<script src="<?php echo base_url() ?>assets/vendors/datatables.net/jquery.dataTables.js"></script>
+	<script src="<?php echo base_url() ?>assets/vendors/datatables.net-bs4/dataTables.bootstrap4.js"></script>
 </body>
